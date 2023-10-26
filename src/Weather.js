32f1ec68ec3ffd9de5b80fe5dcf20b2e42ee000a
svttop <106789592+svttop@users.noreply.github.com>
2023-10-26 14:09:29 +1300

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import FormatDate from "./FormatDate";
 import WeatherInfo from "./WeatherDescription";
 import axios from "axios";
 import "./Weather.css";
