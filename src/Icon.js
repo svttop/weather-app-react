f8494fd87640d2f3b56064bd53da4340b7567efb
svttop <106789592+svttop@users.noreply.github.com>
2023-11-03 16:31:53 +1300

    Update Icon.js

--- src/Icon.js
+++ src/Icon.js
@@ -3,30 +3,30 @@ import ReactAnimatedWeather from "react-animated-weather";
 
 export default function WeatherIcon(props) {
   const codeMapping = {
-    "01d": "CLEAR_DAY",
-    "01n": "CLEAR_NIGHT",
-    "02d": "PARTLY_CLOUDY_DAY",
-    "02n": "PARTLY_CLOUDY_NIGHT",
-    "03d": "PARTLY_CLOUDY_DAY",
-    "03n": "PARTLY_CLOUDY_NIGHT",
-    "04d": "CLOUDY",
-    "04n": "CLOUDY",
-    "09d": "RAIN",
-    "09n": "RAIN",
-    "10n": "RAIN",
-    "10d": "RAIN",
-    "11n": "RAIN",
-    "11d": "RAIN",
-    "13n": "SNOW",
-    "13d": "SNOW",
-    "50n": "FOG",
-    "50d": "FOG",
+    "clear-sky-day": "CLEAR_DAY",
+    "clear-sky-night": "CLEAR_NIGHT",
+    "few-clouds-day": "PARTLY_CLOUDY_DAY",
+    "few-clouds-night": "PARTLY_CLOUDY_NIGHT",
+    "scattered-clouds-day": "CLOUDY",
+    "scattered-clouds-night": "CLOUDY",
+    "broken-clouds-day": "CLOUDY",
+    "broken-clouds-night": "CLOUDY",
+    "rain-day": "RAIN",
+    "rain-night": "RAIN",
+    "thunderstorm-day": "RAIN",
+    "thunderstorm-night": "RAIN",
+    "shower-rain-day": "SLEET",
+    "shower-rain-night": "SLEET",
+    "snow-day": "SNOW",
+    "snow-night": "SNOW",
+    "mist-day": "WIND",
+    "mist-night": "WIND",
   };
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="blue"
-      size={66}
+      color="#202124"
+      size={props.size}
       animate={true}
     />
   );
