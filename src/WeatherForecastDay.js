f9d750de99dc14703752a04b190b7b0a12366705
svttop <106789592+svttop@users.noreply.github.com>
2023-11-03 14:10:20 +1300

    Update WeatherForecastDay.js

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,5 +1,5 @@
 import React from "react";
-import WeatherIcon from "./WeatherIcon";
+import WeatherIcon from "./Icon";
 
 export default function WeatherForecastDay(props) {
   function maxTemperature() {
