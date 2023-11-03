0036bca1284f2094506e246737162434d8dee814
svttop <106789592+svttop@users.noreply.github.com>
2023-11-03 16:22:27 +1300

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -61,7 +61,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
-        <WeatherForecast data={weatherData.city} />
+        <WeatherForecast city={weatherData.city} />
       </div>
     );
   } else {
