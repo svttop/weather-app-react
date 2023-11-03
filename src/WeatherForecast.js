84a91e63871567482a54ad76986d8caef4bb237d
svttop <106789592+svttop@users.noreply.github.com>
2023-11-03 14:58:32 +1300

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
-import ForecastDay from "./ForecastDay";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
