b060bd3e4143b47769d9642d97ff8318ce681fd5
svttop <106789592+svttop@users.noreply.github.com>
2023-11-03 15:21:38 +1300

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,45 +1,45 @@
-import React, { useState, useEffect } from "react";
-import "./WeatherForecast.css";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import WeatherForecastDay from "./WeatherForecastDay";
 
-export default function WeatherForecast(props) {
-  let [loaded, setLoaded] = useState(false);
-  let [forecast, setForecast] = useState(null);
+function WeatherForecast(props) {
+  const [loaded, setLoaded] = useState(true);
+  const [forecast, setForecast] = useState([]);
 
   useEffect(() => {
-    setLoaded(false);
-  }, [props.data.city]);
+    if (!loaded) {
+      let apiKey = "597c40c39084687093b091cd48b366f8";
+      let city = props.data.city;
+      let apiUrl =
+        "https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric;";
 
-  function handleResponse(response) {
-    setForecast(response.data.daily);
-    setLoaded(true);
-  }
+      axios
+        .get(apiUrl)
+        .then((response) => {
+          setForecast(response.data.list);
+          setLoaded(true);
+        })
+        .catch((error) => {
+          console.log(error);
+          setLoaded(false);
+        });
+    }
+  }, [loaded]);
 
   if (loaded) {
     return (
       <div className="WeatherForecast">
         <div className="row mt-5 mb-4">
-          {forecast.map(function (dailyForecast, index) {
-            if (index < 5) {
-              return (
-                <div className="col" key={index}>
-                  <WeatherForecastDay data={dailyForecast} />
-                </div>
-              );
-            } else {
-              return null;
-            }
+          {forecast.slice(0, 5).map(function (dailyForecast, index) {
+            return (
+              <div className="col" key={index}>
+                <WeatherForecastDay data={dailyForecast} />
+              </div>
+            );
           })}
         </div>
       </div>
     );
   } else {
-    let apiKey = "597c40c39084687093b091cd48b366f8";
-    let city = props.data.city;
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
-
-    axios.get(apiUrl).then(handleResponse);
     return null;
   }
 }
