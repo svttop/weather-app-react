da1c0096f8817095dc5560381210bbd294ac56cf
svttop <106789592+svttop@users.noreply.github.com>
2023-11-03 16:02:16 +1300

    Update WeatherDescription.js

--- src/WeatherDescription.js
+++ src/WeatherDescription.js
@@ -6,23 +6,27 @@ import WeatherTemperature from "./Temperature";
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
-      <h1>{props.data.city}</h1>
+      <h1>{props.info.city}</h1>
       <ul>
         <li>
-          <FormatDate date={props.data.date} />
+          <FormatDate date={props.info.date} />
         </li>
-        <li className="text-capitalize">{props.data.description}</li>
+        <li className="text-capitalize">{props.info.description}</li>
       </ul>
       <div className="row mt-3">
-        <div className="col-6">
-          <WeatherIcon code={props.data.icon} />
-          <br />
-          <WeatherTemperature fahrenheit={props.data.temperature} />
+        <div className="col-6 d-flex justify-content-start">
+          <WeatherIcon code={props.info.icon} size={60} />
+          <WeatherTemperature
+            tempC={props.info.temperature}
+            unit={props.unit}
+            setUnit={props.setUnit}
+          />
         </div>
-        <div className="col-6">
+        <div className="col-6 propieties">
           <ul>
-            <li>Humidity: {props.data.humidity}%</li>
-            <li>Wind: {props.data.wind} km/h</li>
+            <li>Humidity: {props.info.humidity}%</li>
+            <li>Wind: {props.info.wind} km/h</li>
+            <li>Pressure: {props.info.pressure} hPa</li>
           </ul>
         </div>
       </div>
